fb5a3a427369090722c9f334b8bb97f6c57fdee1
yundingwu <superlowkey@outlook.com>
2023-04-03 16:37:19 -0700

    Initial Commit

---
+++ EventSearch/EventSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventSearchApp.swift
+//  EventSearch
+//
+//  Created by Desmond Wu on 4/3/23.
+//
+
+import SwiftUI
+
+@main
+struct EventSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
